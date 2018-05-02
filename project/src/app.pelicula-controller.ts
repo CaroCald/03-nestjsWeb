c521c9a4e13611da4e9ecd127f7a673a7cb80847
CaroCald <carolina.2594@hotmail.com>
2018-05-02 16:06:42 -0500

    commit

--- project/src/app.pelicula-controller.ts
+++ project/src/app.pelicula-controller.ts
@@ -1,13 +1,34 @@
-import {Controller, Get, Post} from "@nestjs/common";
+import {Controller, Get, HttpCode, Post} from "@nestjs/common";
+import {Param, Req} from "@nestjs/common/utils/decorators/route-params.decorator";
 
 @Controller('Pelicula')
 export class AppPeliculaController{
+    peliculas: Pelicula[]=[];
     @Get('mostrarCartelera')
     mostrarCartelera(){
         return ['Thor', 'Capi', 'Spidey'] //responder un json
     }
-    @Post('mostrarCartelera')
-    anadirCartelera(){
-        return ['Thor', 'Capi', 'Spidey', 'iron man'] //responder un json
+    @Post('mostrarCartelera/:nombre/:estreno')
+    @HttpCode(203)
+    anadirCarteleraQueryParameter(@Req() req, @Req() res){
+        const parametrosQuery=req.query;
+        this.peliculas.push(new Pelicula(parametrosQuery.nombre, parametrosQuery.estreno));
+        return res.send(this.peliculas);
     }
+
+/*
+anadirCartelera(@Param() parametros){
+        this.peliculas.push(new Pelicula(parametros.nombre, parametros.estreno)); //responder un json
+        return this.peliculas;
+    }
+ */
+
+}
+
+
+class Pelicula{
+    constructor(public nombre?:String,
+    public estreno?: number){
+    }
+
 }
