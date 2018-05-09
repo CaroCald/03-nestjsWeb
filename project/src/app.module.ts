c6b5c77463abf03877ee39fa83bd1a730c3592f3
CaroCald <carolina.2594@hotmail.com>
2018-05-09 15:02:26 -0500

    post y get

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import {AppPeliculaController} from "./app.pelicula-controller";
+import {ParametrosController} from "./parametros.controller";
 
 @Module({
   imports: [], //importar modulos
-  controllers: [AppController, AppPeliculaController], //controladores
+  controllers: [AppController, AppPeliculaController, ParametrosController], //controladores
   components: [], //componentes
 })
 export class AppModule {}
--- project/src/parametros.controller.ts
+++ project/src/parametros.controller.ts
@@ -1,6 +1,37 @@
-import {Controller} from "@nestjs/common";
+import {Body, Controller, Get, Headers, Param, Post, Query, Req, Res} from "@nestjs/common";
 
 @Controller('Parametros')
 export class ParametrosController{
-    
+    @Post('recuperar/:id')
+    recuperarParametros(@Req() request,
+                        @Res() response,
+                        @Param() paramParams,
+                        @Query() queryParams,
+                        @Body() bodyParams){
+
+        const respuesta={
+            paramParams: paramParams,
+            queryParams: queryParams,
+            bodyParams: bodyParams,
+        };
+
+        console.log(respuesta);
+        return response.send(respuesta);
+    }
+    @Get('ResRes')
+    requestResponse(@Req() request, @Res() response, @Headers() headers){
+        const respuesta={
+            baseUrl:request.baseUrl,
+            hostname: request.hostname,
+            subdomains: request.subdomains,
+            ip: request.ip,
+            method: request.method,
+            originalUrl: request.originalUrl,
+            path: request.path,
+            protocol: request.protocol,
+            headers,
+        };
+        console.log(respuesta);
+        return response.redirect(301,'/Pelicula/mostrarCartelera'); //url relativa
+    }
 }
